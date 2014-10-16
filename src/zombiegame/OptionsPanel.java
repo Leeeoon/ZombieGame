5b0d2a33b62f8699c9cac8259dd832fd9d99dd07
GraesonG <gvg5067@gmail.com>
2014-10-15 22:29:07 -0400

    Graeson- Added buttons for OptionsPanel

--- src/zombiegame/OptionsPanel.java
+++ src/zombiegame/OptionsPanel.java
@@ -1,17 +1,27 @@
 package zombiegame;
 
 import javax.swing.*;
-
+import javax.swing.JRadioButton;
+//Updated: 10/15 by Graeson
 public class OptionsPanel extends JPanel {
-   JButton heroButtons;
-   JButton backgroundButton;
-   JRadioButton easy, hard;
-   ButtonGroup bg;
-   JButton beginGameButton;
-   
-   
-   
- static void setValues(){
-     
- }
+
+   void setValues() {
+        JButton heroButtons;                          //If these are out of the setValues() method then they are considered
+        JButton backgroundButton;                     //static and it won't work
+        JRadioButton easy, hard;
+        ButtonGroup bg;                               //What is a button group
+        JButton beginGameButton;
+
+
+        heroButtons = new JButton("Hero");
+        backgroundButton = new JButton("Why?");       //Not sure what this is for
+        add(heroButtons);
+        add(backgroundButton);
+//Button Group
+        easy = new JRadioButton("Easy");
+        hard = new JRadioButton("hard");
+        bg = new ButtonGroup();
+        bg.add(easy);
+        bg.add(hard);
+    }
 }
