8eedf938a29c30398bc485a8cf5dcb581256e11c
GraesonG <gvg5067@gmail.com>
2014-10-16 13:53:26 -0400

    Graeson- In class fix

--- src/zombiegame/TitlePanel.java
+++ src/zombiegame/TitlePanel.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class TitlePanel extends JPanel {
     String welcomeMsg = "Welcome to the Zombie Game";
-    JButton startButton = new JButton ("Start"); 
+    JButton startButton = new JButton ("Start");               // For the initial load/welcome screen
     //add(startButton);                                        //Problem getting this to work
     
 }
