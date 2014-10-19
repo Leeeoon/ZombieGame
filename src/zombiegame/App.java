480b171ee110e6dddfad2e2ae50c00e4f79a3083
Leon <lol5115@psu.edu>
2014-10-19 14:18:15 -0400

    Changed the project description.

--- src/zombiegame/App.java
+++ src/zombiegame/App.java
@@ -1,7 +1,7 @@
 // Programmers: Dana Cinque, Graeson Gehringer, Leon Ling
 // Start Date:  October 9, 2014
 // End Date:    TBD
-// Description: This program will do something.
+// Description: This program will create a frogger style game.
 
 package zombiegame;
 
