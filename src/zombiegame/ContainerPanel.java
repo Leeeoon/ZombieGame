6ba400273e47bd334082334ad85059529a4e7f15
danacinque <danacinque@104.38.28.127>
2014-10-19 16:56:28 -0400

    Setup/layout for titlePanel, optionsPanel; basic setup for gamePanel; fixed up optionsPanel

--- src/zombiegame/ContainerPanel.java
+++ src/zombiegame/ContainerPanel.java
@@ -1,5 +1,6 @@
 package zombiegame;
 
+import java.awt.*;
 import java.awt.Graphics;
 import java.awt.event.*;
 import javax.swing.*;
@@ -19,7 +20,9 @@ public class ContainerPanel extends JPanel implements ActionListener {
         titlePanel = new TitlePanel();
         titlePanel.setLayout(null);
         titlePanel.setBounds(0, 0, 500, 500);
+        
         titlePanel.startButton.setBounds(225, 300, 50, 50);
+        titlePanel.startButton.addActionListener(this);
         titlePanel.add(titlePanel.startButton);
         add(titlePanel);
         // draw the string (titlePanel.welcomeMsg)
@@ -29,6 +32,49 @@ public class ContainerPanel extends JPanel implements ActionListener {
         
         // Set up optionsPanel ----------------------------------------
         optionsPanel = new OptionsPanel(gamePanel);
+        optionsPanel.setVisible(false);
+        optionsPanel.setLayout(null);
+        optionsPanel.setBounds(0, 0, 500, 500);
+        
+        // Hero options
+        // ---- Choose your hero
+        optionsPanel.heroButtons.setBounds(25, 50, 165, 30);
+        optionsPanel.add(optionsPanel.heroButtons);
+        // ---- Hero option 1
+        optionsPanel.hero1.setBounds(200, 40, 50, 50);
+        optionsPanel.add(optionsPanel.hero1);
+        
+        // Background options
+        // ---- Choose background
+        optionsPanel.backgroundButtons.setBounds(25, 130, 165, 30);
+        optionsPanel.add(optionsPanel.backgroundButtons);
+        // ---- Background option 1
+        optionsPanel.background1.setBounds(200, 120, 50, 50);
+        optionsPanel.add(optionsPanel.background1);
+        
+        // Difficulty options
+        optionsPanel.difficultyButtons.setBounds(25, 210, 165, 30);
+        optionsPanel.add(optionsPanel.difficultyButtons);
+        // Difficulty: easy
+        optionsPanel.easy.setBounds(200, 210, 100, 30);
+        optionsPanel.add(optionsPanel.easy);
+        // Difficulty: hard
+        optionsPanel.hard.setBounds(300, 210, 100, 30);
+        optionsPanel.add(optionsPanel.hard);
+        
+        // Begin game button
+        optionsPanel.beginGameButton.setBounds(200, 300, 100, 75);
+        optionsPanel.add(optionsPanel.beginGameButton);
+        optionsPanel.beginGameButton.addActionListener(this);
+        
+        add(optionsPanel);
+        // ------------------------------------------------------------
+        
+        // Set up game panel ------------------------------------------
+        gamePanel.setLayout(null);
+        gamePanel.setVisible(false);
+        gamePanel.setBounds(0, 0, 500, 400);
+        add(gamePanel);
         
     }
     
@@ -50,6 +96,21 @@ public class ContainerPanel extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
     
+        if (e.getSource() == titlePanel.startButton)
+        {
+            titlePanel.setVisible(false);
+            System.out.println("Title panel invisible");
+            optionsPanel.setVisible(true);
+            System.out.println("Options panel visible");
+        }
+        
+        if (e.getSource() == optionsPanel.beginGameButton)
+        {
+            optionsPanel.setVisible(false);
+            System.out.println("Options panel invisible");
+            gamePanel.setVisible(true);
+            System.out.println("Game panel visible");
+        }
     
     }
     
--- src/zombiegame/OptionsPanel.java
+++ src/zombiegame/OptionsPanel.java
@@ -6,25 +6,32 @@ import javax.swing.*;
 public class OptionsPanel extends JPanel {
 
     JButton heroButtons;                          //If these are out of the setValues() method then they are considered
-    JButton backgroundButton;                     //static and it won't work
+    JButton backgroundButtons;                     //static and it won't work
     JRadioButton easy, hard;
-    ButtonGroup bg;
+    ButtonGroup difficulty; // Renamed from "bg" to "difficulty" to avoid confusion
     JButton beginGameButton;
+    JButton difficultyButtons;
+    
+    // Adding separate buttons for the hero/background images - Dana
+    JButton hero1; // will add more later
+    JButton background1; // will add more later
 
     public OptionsPanel(GamePanel gamePanel) {
         beginGameButton = new JButton("Begin Game");
-        heroButtons = new JButton("Hero");
-        backgroundButton = new JButton("Background Button");       //Not sure what this is for
-
+        heroButtons = new JButton("Choose your hero:");
+        hero1 = new JButton("1");
+        background1 = new JButton("1");
+        backgroundButtons = new JButton("Choose background:");       //Not sure what this is for
 //---------------------------------------------------Button Group
+        difficultyButtons = new JButton("Choose difficulty:");
         easy = new JRadioButton("Easy");
-        hard = new JRadioButton("hard");
-        bg = new ButtonGroup();
+        hard = new JRadioButton("Hard");
+        /*difficulty = new ButtonGroup();
+        difficulty.add(easy);
+        difficulty.add(hard);*/
 //---------------------------------------------------Adding the buttons        
-        add(heroButtons);
-        add(backgroundButton);
-        bg.add(easy);
-        bg.add(hard);
+        //add(heroButtons);
+        //add(backgroundButtons);
 
 
     }
